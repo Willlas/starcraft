265c81fabb3a422b39965c7742ccbc82dd21b795
FACTUM-CLOUD\guillermo.pastor <guillermo.pastor@factum.es>
2018-06-27 13:59:12 +0200

    apm calculate per keypress => smooth apm per sec

--- starcraft/imports/ui/components/starcraft/starcraft.js
+++ starcraft/imports/ui/components/starcraft/starcraft.js
@@ -10,7 +10,13 @@ Template.starcraft.onCreated(function () {
     }]);
     this.hello = new ReactiveVar();
     this.hello.set('hello there');
+    this.apm = new ReactiveVar();
+    this.apm.set(0);
 
+});
+
+Template.starcraft.onRendered(function(){
+    this.find('textarea').focus();
 });
 
 Template.starcraft.helpers({
@@ -19,17 +25,35 @@ Template.starcraft.helpers({
     },
     hello: function () {
         return Template.instance().hello.get('hello');
+    },
+    APM: function(){
+        return Template.instance().apm.get('apm');
     }
 });
 
 Template.starcraft.events({
     'keydown textarea': function (e) {
-        var clonedTable = Template.instance().table.get('table');
+
+        //allow tabing in textarea
+        if(e.which == 9){
+            e.preventDefault();
+        }
+        let clonedTable = Template.instance().table.get('table');
+
         clonedTable.push({
                 'key': e.which,
                 'moment': Date.now() - clonedTable[0].moment
             });
+
         Template.instance().table.set(clonedTable);
+
+        //var clonedAPM = Template.instance().apm.get('apm');
+
+        Template.instance().apm.set(
+            (clonedTable.length /
+                ((clonedTable[clonedTable.length -1].moment - clonedTable[1].moment + 1)/1000))
+                * 60
+        );
         console.log(e.which, Date.now());
     }
 });
